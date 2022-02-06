d0d595c9c88f4381b2fa23b0bfc5239e8d164d93
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-02-06 14:12:21 +0100

    feat: Ask the GPU to multiply elements in an array, using a shader
    See the Vulkano Compute guide;
    - https://vulkano.rs/guide/compute-intro
    - https://vulkano.rs/guide/compute-pipeline
    - https://vulkano.rs/guide/descriptor-sets
    - https://vulkano.rs/guide/dispatch

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::sync::Arc;
+
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
 use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, PrimaryCommandBuffer};
+use vulkano::descriptor_set::layout::DescriptorSetLayout;
+use vulkano::descriptor_set::PersistentDescriptorSet;
+use vulkano::pipeline::{ComputePipeline, Pipeline, PipelineBindPoint};
+use vulkano::sync;
 use vulkano::sync::GpuFuture;
 
 use crate::engine::Engine;
@@ -11,6 +17,7 @@ fn main() {
 
     let engine = Engine::new();
 
+    // region Copy data from one buffer to another
     println!("\nDemo: Copy data from one buffer to another.");
 
     // Source contents: 0, 1, 2, 3, … 63 (size 64)
@@ -26,7 +33,7 @@ fn main() {
         .expect("Failed to create destination buffer");
 
     // Create a command buffer…
-    let mut builder = AutoCommandBufferBuilder::primary(engine.device.clone(), engine.graphics_queue.family(), CommandBufferUsage::MultipleSubmit)
+    let mut builder = AutoCommandBufferBuilder::primary(engine.device.clone(), engine.graphics_queue.clone().family(), CommandBufferUsage::MultipleSubmit)
         .expect("Could not create buffer builder.");
     // …that copies the buffers…
     builder.copy_buffer(source.clone(), destination.clone())
@@ -36,7 +43,7 @@ fn main() {
         .expect("Could not build buffer.");
 
     // We can now start it.
-    let finished = command_buffer.execute(engine.graphics_queue)
+    let finished = command_buffer.execute(engine.graphics_queue.clone())
         .expect("Could not execute the command buffer.");
     // Wait until it is done…
     finished.then_signal_fence_and_flush().expect("Could not fence and flush the executed buffer.")
@@ -46,4 +53,91 @@ fn main() {
     // We can now read the destination buffer.
     let updated_destination = destination.read().expect("Could not read from the destination buffer");
     println!("Destination contents (after):  {:?}", &*updated_destination);
+    // endregion
+
+    // region Multiply an array by 12 in a single operation
+    println!("\nDemo: Multiply an array of size 65536 by 12 in a single operation");
+
+    let data = 0..65536;
+    let data_buffer = CpuAccessibleBuffer::from_iter(engine.device.clone(), BufferUsage::all(), false, data)
+        .expect("Failed to create buffer.");
+
+    // Load the shader defined below
+    let shader = times_twelve::load(engine.device.clone())
+        .expect("Failed to load shader module.");
+
+    // Create a compute pipeline
+    // This pipeline represents "the order of executing the shader"
+    let compute_pipeline = ComputePipeline::new(
+        engine.device.clone(),
+        shader.entry_point("main").expect("Couldn't find entry point 'main' in shader"),
+        &(),
+        None,
+        |_| {},
+    ).expect("Failed to create compute pipeline.");
+
+    // Represents the necessary data layout given as parameter 0 to the compute pipeline
+    let layout: Arc<DescriptorSetLayout> = compute_pipeline.layout().descriptor_set_layouts().get(0)
+        .expect("Couldn't find layout descriptor 0.")
+        .clone();
+
+    // Creates a descriptor set that describes how the data is fed to the compute pipeline
+    let mut set_builder = PersistentDescriptorSet::start(layout.clone());
+    set_builder.add_buffer(data_buffer.clone()).expect("Could not add data buffer.");
+    let set = set_builder.build().expect("Could not build the descriptor set");
+
+    let mut command_builder = AutoCommandBufferBuilder::primary(
+        engine.device.clone(),
+        engine.graphics_queue.clone().family(),
+        CommandBufferUsage::OneTimeSubmit,
+    ).expect("Could not create command builder.");
+
+    command_builder.bind_pipeline_compute(compute_pipeline.clone());
+    command_builder.bind_descriptor_sets(
+        PipelineBindPoint::Compute,
+        compute_pipeline.layout().clone(),
+        0,
+        set,
+    );
+    // The pipeline will be dispatched over 1024 work groups, in a single dimension
+    command_builder.dispatch([1024, 1, 1]).expect("Could not dispatch the command builder");
+
+    let command_buffer = command_builder.build().expect("Could not build the command builder");
+
+    // Tell the GPU to start the pipeline
+    let future = sync::now(engine.device.clone())
+        .then_execute(engine.graphics_queue.clone(), command_buffer)
+        .expect("Could not execute the command buffer")
+        .then_signal_fence_and_flush()
+        .expect("Could not fence and flush");
+
+    future.wait(None).expect("The command buffer never finished");
+
+    let content = data_buffer.read().unwrap();
+    for (n, val) in content.iter().enumerate() {
+        assert_eq!(*val, n as u32 * 12);
+    }
+    println!("The product succeeded!");
+
+    // endregion
+}
+
+mod times_twelve {
+    vulkano_shaders::shader! {
+        ty: "compute",
+        src: "
+            #version 450
+
+            layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+
+            layout(set = 0, binding = 0) buffer Data {
+                uint data[];
+            } buf;
+
+            void main() {
+                uint idx = gl_GlobalInvocationID.x;
+                buf.data[idx] *= 12;
+            }
+        "
+    }
 }
