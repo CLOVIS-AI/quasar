48430b6c5814b463b181627bdded3f88bc4ce906
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-04-02 15:14:48 +0200

    feat: Created skeleton of the World package

---
+++ engine/src/world/coordinates.rs
@@ -0,0 +1,5 @@
+pub struct Coords {
+    pub x: usize,
+    pub y: usize,
+    pub z: usize,
+}
---
+++ engine/src/world/world.rs
@@ -0,0 +1,5 @@
+use crate::world::object::Object;
+
+pub struct World {
+    objects: Vec<Object>,
+}
